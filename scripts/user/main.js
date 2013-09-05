fd03356ca68d220e21bba988f92aeb5e30164487
Angel <angelinkas@inbox.ru>
2013-09-05 12:38:55 +0400

    edit

---
+++ scripts/user/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    // stars rating
+    $(function(){
+        $('.rating').rating({
+            fx: 'full',
+            image: 'images/stars2.png',
+            loader: 'images/ajax-loader.gif',
+            callback: function(responce){
+
+                this.vote_success.fadeOut(2000);
+            }
+        });
+    });
+
+});
